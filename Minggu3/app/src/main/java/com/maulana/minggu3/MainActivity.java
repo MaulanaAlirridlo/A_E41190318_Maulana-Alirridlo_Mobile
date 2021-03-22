31bbcd0ca05feb4a857830f58116a9a53f4f203f
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-03-22 09:54:02 +0700

    Add Tugas Minggu 3 Layout, Material Design dan Scroll View

---
+++ Minggu3/app/src/main/java/com/maulana/minggu3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.maulana.minggu3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Pada, Subject, Pesan;
+    Button Submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tugas);
+
+        Pada = findViewById(R.id.pada);
+        Subject = findViewById(R.id.subject);
+        Pesan = findViewById(R.id.pesan);
+        Submit = findViewById(R.id.submit);
+    }
+}
